3db322f7898a4c90cf4ce3dad685f868983bff68
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2022-12-04 20:05:04 +0100

    fixed previous install script

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ pkg install wget curl proot tar screen openssh -y
 sshd
 
 mkdir $HOME/ubuntu
-cp ./afterUbuntuSetup.sh $HOME/ubuntu
-cp ./themes $HOME/ubuntu
-cp ./icons $HOME/ubuntu
 cd $HOME/ubuntu
 wget https://raw.githubusercontent.com/tuanpham-dev/termux-ubuntu/master/ubuntu.sh
 chmod +x ubuntu.sh
