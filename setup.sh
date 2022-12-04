f2d46b8a252303c46b0912923248de51c21a5e43
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2022-12-04 19:58:05 +0100

    removed xfce styling

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "------------- installing utilities------------"
+
+sudo apt install git -y
+sudo apt install wget -y
+sudo apt install curl -y
+sudo apt install htop -y
+sudo apt install neofetch -y
+sudo apt install unzip -y
+sudo apt install chromium-browser -y
+sudo apt install mysql-server -y
+sudo apt install python3-pip
+
+curl -fsSL https://code-server.dev/install.sh | sh
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt install nodejs -y
+
+
+echo "------------- setting up directories --------------"
+
+mkdir $HOME/Projects
+mkdir $HOME/University
